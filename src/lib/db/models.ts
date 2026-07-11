c1631e0de17f6fc54a8c037cd1a28f73998dccb2
Pranay B <b.pranay.git@gmail.com>
2026-07-11 13:39:50 +0530

    feat(db): add models file

---
+++ src/lib/db/models.ts
@@ -0,0 +1,8 @@
+// src/lib/db/models.ts
+import "@/models/User";
+import "@/models/Video";
+import "@/models/Place";
+import "@/models/Media";
+import "@/models/SavedVideo";
+import "@/models/Itinerary";
+// Import any other models you have
