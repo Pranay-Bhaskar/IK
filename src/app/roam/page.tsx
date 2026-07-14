1c963d4091eefa8116b9eaba432073f1bf1887fa
Pranay B <b.pranay.git@gmail.com>
2026-07-14 16:35:11 +0530

    Improve roam page feed and interactions

--- src/app/roam/page.tsx
+++ src/app/roam/page.tsx
@@ -1,3 +1,4 @@
+/*
 import RoamFeed from '@/components/RoamFeed';
 
 export const metadata = {
@@ -7,6 +8,22 @@ export const metadata = {
 export default function RoamPage() {
   return (
     <main className="bg-black min-h-screen">
+      <RoamFeed />
+    </main>
+  );
+}
+*/
+
+
+import RoamFeed from '@/components/RoamFeed';
+
+export const metadata = {
+  title: 'Roam - Discover Locations',
+};
+
+export default function RoamPage() {
+  return (
+    <main className="relative min-h-dvh bg-black overflow-hidden">
       <RoamFeed />
     </main>
   );
